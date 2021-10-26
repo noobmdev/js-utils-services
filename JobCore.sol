ed94c5bf106720ac02b2a500e09549250622ad87
manhtv <manhtv@rikkeisoft.com>
2021-10-26 17:25:54 +0700

    update jobCore contract

--- JobCore.sol
+++ JobCore.sol
@@ -158,6 +158,8 @@ contract JobCore is Ownable {
     Job[] public jobs;
     // jobId => owner
     mapping(uint256 => address) public jobOwner;
+    // owner => uint256[]
+    mapping(address => uint256[]) public ownerJobs;
     
     // address => Resume[]
     mapping(address => Resume[]) resumes;
@@ -165,24 +167,74 @@ contract JobCore is Ownable {
     mapping(uint256 => address) public resumeOwner;
     // resumeId => resumeIndex
     mapping(uint256 => uint256) public resumeIndexs;
+    // address => Resume
+    mapping(address => Resume) public currentResume;
     
     // jobId => resumeIds[]
-    mapping(uint256 => uint256[]) appliedResumes;
+    mapping(uint256 => uint256[]) public appliedResumes;
     // address => jobIds[]
-    mapping(address => uint256[]) appliedJobs;
+    mapping(address => uint256[]) public appliedJobs;
 
     /* 
     / === CONSTRUCTOR
     */
     constructor() {
+        address[8] memory recruiterAddresses = [
+                0x4Acf773BD581BdF5De5F9E6fc06589C3F6C791F9,
+                0x4Acf773BD581BdF5De5F9E6fc06589C3F6C791F9,
+                0x4Acf773BD581BdF5De5F9E6fc06589C3F6C791F9,
+                0x4Acf773BD581BdF5De5F9E6fc06589C3F6C791F9,
+                 0x4Acf773BD581BdF5De5F9E6fc06589C3F6C791F9,
+                    0x4Acf773BD581BdF5De5F9E6fc06589C3F6C791F9,
+                    0x4Acf773BD581BdF5De5F9E6fc06589C3F6C791F9,
+                    0x4Acf773BD581BdF5De5F9E6fc06589C3F6C791F9
+            ];
+        string[8] memory names = ["Company 1", "Company 2", "Company 3", "Company 4", "Company 5", "Company 6", "Company 7", "Company 8"];
+        string[8] memory headquarters = ["Headquarter 1", "Headquarter 2", "Headquarter 3", "Headquarter 4", "Headquarter 5", "Headquarter 6", "Headquarter 7", "Headquarter 8"];
+        string[8] memory companySizes = ["1-10", "10-50", "1-10", "1-10", "10-50", "50-100", "1-10", "1-10"];
+        string[8] memory websites = ["https://web1.com", "https://web2.com", "https://web3.com", "https://web4.com", "https://web5.com", "https://web6.com", "https://web7.com", "https://web8.com"];
+        string[8] memory contacts = ["Contact 1", "Contact 2", "Contact 3", "Contact 4", "Contact 5", "Contact 6", "Contact 7", "Contact 8"];
+        string[8] memory addresses = ["Address 1", "Address 2", "Address 3", "Address 4", "Address 5", "Address 6", "Address 7", "Address 8"];
+        string[8] memory logos = [
+                "https://res.cloudinary.com/munumber2/image/upload/v1634401209/NHATUYENDUNG1_vtcgeq.png",
+                "https://res.cloudinary.com/munumber2/image/upload/v1634401208/NHATUYENDUNG2_j4mhca.png", 
+                "https://res.cloudinary.com/munumber2/image/upload/v1634401209/NHATUYENDUNG3_utjokp.png", 
+                "https://res.cloudinary.com/munumber2/image/upload/v1634401209/NHATUYENDUNG4_h1djot.png", 
+                "https://res.cloudinary.com/munumber2/image/upload/v1634401209/NHATUYENDUNG5_w7xwkp.png", 
+                "https://res.cloudinary.com/munumber2/image/upload/v1634401209/NHATUYENDUNG6_xvzgd8.png", 
+                "https://res.cloudinary.com/munumber2/image/upload/v1634401209/NHATUYENDUNG7_ifgvt0.png", 
+                "https://res.cloudinary.com/munumber2/image/upload/v1634401209/NHATUYENDUNG6_xvzgd8.png"
+            ];
         
+        
+        for(uint8 i = 0; i < names.length; i++) {
+            addRecruiter(recruiterAddresses[i], names[i], headquarters[i], companySizes[i], websites[i], contacts[i], addresses[i], logos[i]);
+        }
     }
     
     /* 
     / MODIFIERS
     */
     modifier onlyRecuiter {
-        require(recruiterToId[msg.sender] != 0, "INVALID_RECRUITER");
+        require(recruiterToId[msg.sender] != 0, "RECRUITER: INVALID_RECRUITER_ADDRESS");
+        _;
+    }
+    
+    modifier isValidRecruiterId(uint256 _recruiter) {
+         uint256 _latestRecruiterId = latestRecruiterId.current();
+        require(_recruiter == 0 || _recruiter >= _latestRecruiterId, "RECRUITER: INVALID_ID");
+        _;
+    }
+    
+    modifier isValidJobId(uint256 _jobId) {
+         uint256 _latestJobId = latestJobId.current();
+        require(_jobId == 0 || _jobId >= _latestJobId, "JOB: INVALID_ID");
+        _;
+    }
+    
+    modifier isValidResumeId(uint256 _resume) {
+         uint256 _latestResumeId = latestResumeId.current();
+        require(_resume == 0 || _resume >= _latestResumeId, "RESUME: INVALID_ID");
         _;
     }
     
@@ -237,6 +289,7 @@ contract JobCore is Ownable {
         }));
         
         jobOwner[_latestJobId] = msg.sender;
+        ownerJobs[msg.sender].push(_latestJobId);
         
         return _latestJobId;
     }
@@ -244,6 +297,10 @@ contract JobCore is Ownable {
     function getLatestJobId() view public returns(uint256) {
         uint256 _latestJobId = latestJobId.current();
         return _latestJobId;
+    }
+    
+    function getOwnerJobs() view public returns(uint256[] memory) {
+        return ownerJobs[msg.sender];
     }
     
     // function getJobs() public returns(Job[] memory) {
@@ -271,11 +328,54 @@ contract JobCore is Ownable {
         return _latestResumeId;
     } 
     
+    function updateCurrentResume(string memory _url) public {
+        currentResume[msg.sender] = Resume({
+            id: 0,
+            url: _url
+        });
+    }
+    
+    function isAppliedJob(uint256 _jobId) view public returns(bool) {
+        uint256[] memory _appliedJobs = appliedJobs[msg.sender];
+        for(uint256 i = 0; i < _appliedJobs.length; i++) {
+            if(_jobId == _appliedJobs[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    function isResumeApplied(uint256 _jobId, uint256 _resumeId) view public returns(bool) {
+        uint256[] memory _appliedResumes = appliedResumes[_jobId];
+        for(uint256 i = 0; i < _appliedResumes.length; i++) {
+            if(_resumeId == _appliedResumes[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    
+    function applyJob(uint256 _jobId, uint256 _resumeId) public isValidJobId(_jobId) isValidResumeId(_resumeId) {
+        require(!isAppliedJob(_jobId), "JOB: APPLIED_JOB");
+        require(!isResumeApplied(_jobId, _resumeId), "JOB: APPLIED_JOB");
+        
+        appliedResumes[_jobId].push(_resumeId);
+        appliedJobs[msg.sender].push(_jobId);
+    }
+    
     function getOwnerResumes() view public returns(Resume[] memory) {
         return resumes[msg.sender];
     }
     
-    function getAppliedResumes() view public returns(Resume[] memory) {
-        
+    function getAppliedResumeIds(uint256 _jobId) view public returns(uint256[] memory) {
+         return appliedResumes[_jobId];
     }
+    
+    function getResumeById(uint256 _resumeId) view public isValidResumeId(_resumeId) returns(Resume memory) {
+        address owner = resumeOwner[_resumeId];
+        uint256 idx = resumeIndexs[_resumeId];
+        return resumes[owner][idx];
+    }
+    
 }
